e242eb1214b22c8458d9292bfb8b7b5a89c184e5
vtsai006 <49217659+vtsai006@users.noreply.github.com>
2019-05-02 21:04:07 -0700

    Update base.hpp

--- base.hpp
+++ base.hpp
@@ -1,16 +1,19 @@
-#ifndef __BASE_HPP__
-#define __BASE_HPP__
-
+#ifndef BASE_HPP
+#define BASE_HPP
 #include <string>
-
 class Base {
     public:
+	Base* left;
+	Base* right;
         /* Constructors */
-        Base() { };
+        Base() {
+	    left = 0;
+	    right = 0;
+        };
 
         /* Pure Virtual Functions */
         virtual double evaluate() = 0;
         virtual std::string stringify() = 0;
 };
 
-#endif //__BASE_HPP__
+#endif
